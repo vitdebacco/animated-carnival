e25561267e87b0fc2243a17d04b98abe63505897
Vit DeBacco <47540556+vitdebacco@users.noreply.github.com>
2021-02-05 21:14:22 -0500

    chore: initial Spring Boot project commit (#1)
    This commit adds the intial Spring Boot project and exposes all Actuator
    endpoints.

---
+++ monitor/build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.4.2"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.4.21"
+	kotlin("plugin.spring") version "1.4.21"
+	id("org.jmailen.kotlinter") version "3.3.0"
+}
+
+group = "com.envylabs"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_15
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "15"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ monitor/src/test/kotlin/com/envylabs/monitor/MonitorApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.envylabs.monitor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MonitorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
